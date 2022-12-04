5c91c372c30397783936975a4331c862ec67d415
Fernando Andrade <fernandofreamunde@gmail.com>
2022-12-04 16:12:46 +0000

    :sparkles: allow to create users

--- src/index.js
+++ src/index.js
@@ -1,21 +1,38 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+ const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+
+  const existing = users.find(user => user.username == username);
+
+  if (existing) {
+    return response.status(400).send("Username already exists!");
+  }
+
+  const newUser = {
+    id: uuidv4(),
+    name, 
+    username, 
+    todos: []
+  }
+
+  users.push(newUser);
+
+  return response.status(201).json(newUser);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
